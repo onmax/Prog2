f2347ff9b43acbbe94f6d52128285bd75c6bcfb7
Máximo J. García Martínez <maximo.garcia.martinez@alumnos.upm.es>
2017-02-16 11:45:59 +0100

    Corregido en clase

--- Objeto_basico/Triangulo_main.java
+++ Objeto_basico/Triangulo_main.java
@@ -2,9 +2,12 @@
 public class Triangulo_main{
 	public static void main(String [] args){
 		Triangulo_obj triangulo;
-		triangulo = new Triangulo_obj(100, 50);
+		triangulo = new Triangulo_obj(25, 25);
 		double area = triangulo.area();
 		
+		System.out.println(triangulo.toString());
 		System.out.println(area);
+		System.out.println(triangulo.esIgual());
+		
 	}
 }
--- Objeto_basico/Triangulo_obj.java
+++ Objeto_basico/Triangulo_obj.java
@@ -1,12 +1,29 @@
 public class Triangulo_obj{
-	double altura, base;
+	double altura, base;	
 	
-	public Triangulo_obj(double x, double y){
-		altura = x;
-		base = y;
+	public Triangulo_obj(double altura, double base) {
+		setAltura(altura);
+		setBase(base);
+	}
+
+	public void setAltura(double altura){
+		this.altura = altura;
 	}
 	
+	public void setBase(double base){
+		this.base = base;
+	}
+	
+	public String toString(){
+		return "[" + base + ", " + altura + "]";
+	}
+	
+	
 	public double area(){
-		return (base + altura)/2;
+		return (base * altura)/2;
+	}
+
+	public boolean esIgual(){
+		return base == altura;
 	}
 }
